2b756fc8c41a3ff88193fd751cd1036ed7b1add1
wilsonfreitas <wilson.freitas@gmail.com>
2022-03-09 07:23:11 -0300

    First commit

---
+++ R/flatforwardCOPOM-package.R
@@ -0,0 +1,16 @@
+#' Flat forward interpolation considering COPOM meetings
+#'
+#' The package \code{flatforwardCOPOM} implements flatforwardCOPOM
+#' interpolation to spot rate curves.
+#' This interpolation considers that the interest rate between COPOM meetings
+#' in Brazil are flat, instead of being flat betweet futures maturities.
+#'
+#' @name flatforwardCOPOM-package
+#' @aliases flatforwardCOPOM
+#' @author Wilson Freitas \email{wilson.freitas@gmail.com}
+#' @references Marcos C. S. Carreira and Richard J. Brostowicz.
+#'             Brazilian Derivatives and Securities, Palgrave Macmillan, 2016
+#' @import fixedincome
+#' @docType package
+NULL
+
